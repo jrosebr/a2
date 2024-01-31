3075eed13461c6a0d4842cac1467914ed603ba9c
jrosebr <jrosebr@iu.edu>
2024-01-31 12:29:47 -0500

    Update a2.rkt

--- a2.rkt
+++ a2.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
 ;Problem 1
+(define list-ref
+  (λ (lst dex)
+    (cond
+      ((empty? lst)
+       (if (> 0 dex)
+           ;then
+           'bad-data
+           ;else
+           
